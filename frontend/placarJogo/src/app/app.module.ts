09b4460ae6a891a0999da23dd55f15e611d6a4ac
Samuel <samuelreboucas07@hotmail.com>
2019-08-24 20:55:25 -0300

    Finalização de tela inicial.

--- frontend/placarJogo/src/app/app.module.ts
+++ frontend/placarJogo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule,
     MainModule,
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
