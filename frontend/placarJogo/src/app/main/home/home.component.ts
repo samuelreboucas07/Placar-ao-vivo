1199a19c7724858cfb2c92d270b94c452c6ba973
Samuel <samuelreboucas07@hotmail.com>
2019-10-23 00:04:48 -0300

    Atualizando home

--- frontend/placarJogo/src/app/main/home/home.component.ts
+++ frontend/placarJogo/src/app/main/home/home.component.ts
@@ -5,7 +5,7 @@ import { HomeServices } from './../../_services/home.service';
 import { Router, ActivatedRoute, RouterEvent, NavigationEnd } from '@angular/router';
 import { catchError, filter } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-
+import { WebsocketServices } from './../../_services/websocket.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -19,9 +19,13 @@ export class HomeComponent implements OnInit {
               public elementRef: ElementRef,
               private homeServices: HomeServices,
               private route: ActivatedRoute,
-              private router: Router) {}
+              private router: Router,
+              private websocketServices: WebsocketServices) {}
   
   ngOnInit() {
+    this.websocketServices.listen('score').subscribe((data) => {
+      this.matches = data['matches']
+    })
   	this.homeServices.getMatches()
   	.pipe(catchError(error => {
   		return throwError(error);
